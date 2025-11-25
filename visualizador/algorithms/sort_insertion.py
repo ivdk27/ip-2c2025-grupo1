60e2be7810e6c33797c69b0435a8a0ee0b4f61ab
Gabriel <99836347+Gabontt@users.noreply.github.com>
2025-11-25 17:08:45 -0300

    Add files via upload

--- visualizador/algorithms/sort_insertion.py
+++ visualizador/algorithms/sort_insertion.py
@@ -2,20 +2,33 @@
 
 items = []
 n = 0
-i = 0      # elemento que queremos insertar
-j = None   # cursor de desplazamiento hacia la izquierda (None = empezar)
+i = 1        # empezamos desde el segundo elemento
+j = None
 
 def init(vals):
     global items, n, i, j
     items = list(vals)
     n = len(items)
-    i = 1      # común: arrancar en el segundo elemento
+    i = 1
     j = None
 
 def step():
-    # TODO:
+    global items, n, i, j
     # - Si i >= n: devolver {"done": True}.
+    if i >= n:
+        return {"done": True}
     # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
+    if j == None:
+        j = i
+        return {"a": j, "b": j, "swap": False, "done": False}
     # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
+    while j > 0 and items[j-1] > items[j]:
+        items[j-1], items[j] = items[j], items[j-1]
+        out = {"a":j-1, "b":j, "swap":True, "done":False}
+        j=j-1
+        return out
     # - Si ya no hay que desplazar: avanzar i y setear j=None.
+    i =i+1
+    j = None
+    return {"a": i, "b": i, "swap": False, "done": False}
     return {"done": True}
